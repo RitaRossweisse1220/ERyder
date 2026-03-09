adabfe6feaf033683ebf6b115895f3c5848050f4
RitaRossweisse1220 <irisyu902@163.com>
2026-03-09 18:11:41 +0800

    Add files via upload

--- ERyder.java
+++ ERyder.java
@@ -1,52 +1,49 @@
 public class ERyder {
-    // 私有变量
     private String bikeID;
     private int batteryLevel;
     private boolean isAvailable;
-    private double kmDriven;
-    public ERyder() {
-        bikeID = "";
-        batteryLevel = 0;
-        isAvailable = true;
-        kmDriven = 0.0;
-    }
-    public ERyder(String bikeID, int batteryLevel, boolean isAvailable, double kmDriven) {
-        this.bikeID = bikeID;
-        this.batteryLevel = batteryLevel;
-        this.isAvailable = isAvailable;
-        this.kmDriven = kmDriven;
-    }
-    public void setBatteryLevel(int batteryLevel) {
-        if(batteryLevel >= 0 && batteryLevel <= 100) {
-            this.batteryLevel = batteryLevel;
+    private double Kmdriven;
+public ERyder() {
+    bikeID = "";
+    batteryLevel = 100;
+    isAvailable = true;
+    Kmdriven = 0.0;
+}
+public ERyder(String bikeID,int batteryLevel,boolean isAvailable,double Kmdriven){
+    this.bikeID=bikeID;
+    this.batteryLevel=batteryLevel;
+    this.isAvailable=isAvailable;
+    this.Kmdriven=Kmdriven;
+}
+public void  setbatterylevel(int batteryLevel){
+        if (batteryLevel >=0 && batteryLevel <=100){
+            this.batteryLevel=batteryLevel;
         }
     }
-    public String getBikeID() {
-        return bikeID;
+public String getbikeID(){
+    return bikeID;
+}
+    public int getbatteryLevel(){
+    return batteryLevel;
+}
+    public boolean isAvailable(){
+    return isAvailable;
+}
+    public double getKmdriven(){
+    return Kmdriven;
+}    
+public void ride(){
+    if(isAvailable && batteryLevel > 0){
+        System.out.println("bike s available");
+
+}
+else{
+    System.out.println("bike is not available");                            
     }
-    
-    public int getBatteryLevel() {
-        return batteryLevel;
-    }
-    
-    public boolean getIsAvailable() {
-        return isAvailable;
-    }
-    
-    public double getKmDriven() {
-        return kmDriven;
-    }
-    public void ride() {
-        if(batteryLevel > 0 && isAvailable) {
-            System.out.println("Bike is available for ride");
-        } else {
-            System.out.println("Bike is not available");
-        }
-    }
-    public void printBikeDetails() {
-        System.out.println("Bike ID: " + bikeID);
-        System.out.println("Battery Level: " + batteryLevel + "%");
-        System.out.println("Available: " + isAvailable);
-        System.out.println("Km Driven: " + kmDriven + " km");
+public void printBikeDetails(){
+    System.out.println("bikeID: " + bikeID);
+    System.out.println("batteryLevel: " + batteryLevel);
+    System.out.println("isAvailable: " + isAvailable);
+    System.out.println("Kmdriven: " + Kmdriven);
     }
 }
